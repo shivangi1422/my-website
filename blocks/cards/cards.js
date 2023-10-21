a1438d75006a72cda21a3482a6c3fe9fa34e7e45
Shivangi Singh <shivangi.singh@gspann.com>
2023-10-21 16:21:55 +0530

    styleChnages

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,13 +2,20 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   /* change to ul, li */
-  const ul = document.createElement('ul');
+  const ul = document.createElement('div');
   [...block.children].forEach((row) => {
-    const li = document.createElement('li');
+    const li = document.createElement('div');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      else div.className = 'text';
+      if(div.className === 'text') {
+        const h3Element = document.createElement('h3');
+        div.insertBefore(h3Element, div.firstChild);
+        h3Element.classList.add('title');
+      }
+      //const h3Element = div.querySelector('h3');
+      //if (h3Element) h3Element.classList.add('title');
     });
     ul.append(li);
   });
